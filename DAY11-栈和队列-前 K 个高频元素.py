86ca899c59c2cbab4d955d96f5b844259c575cbe
Chen Shuai <47770924+zhizhuodemao@users.noreply.github.com>
2024-02-05 20:54:22 +0800

    Add files via upload

---
+++ DAY11-栈和队列-前 K 个高频元素.py
@@ -0,0 +1,42 @@
+"""
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+输入: nums = [1], k = 1
+输出: [1]
+提示：
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 $O(n \log n)$ , n 是数组的大小。
+题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+你可以按任意顺序返回答案。
+#算法公开课
+"""
+# 时间复杂度：O(nlogk)
+# 空间复杂度：O(n)
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 要统计元素出现频率
+        map_ = {}  # nums[i]:对应出现的次数
+        for i in range(len(nums)):
+            map_[nums[i]] = map_.get(nums[i], 0) + 1
+
+        # 对频率排序
+        # 定义一个小顶堆，大小为k
+        pri_que = []  # 小顶堆
+
+        # 用固定大小为k的小顶堆，扫描所有频率的数值
+        for key, freq in map_.items():
+            heapq.heappush(pri_que, (freq, key))
+            if len(pri_que) > k:  # 如果堆的大小大于了K，则队列弹出，保证堆的大小一直为k
+                heapq.heappop(pri_que)
+
+        # 找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
+        result = [0] * k
+        for i in range(k - 1, -1, -1):
+            result[i] = heapq.heappop(pri_que)[1]
+        return result
