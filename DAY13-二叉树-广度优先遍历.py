594e3a384d483748759c0255173f411d6b846843
Chen Shuai <47770924+zhizhuodemao@users.noreply.github.com>
2024-02-08 00:09:35 +0800

    Add files via upload

---
+++ DAY13-二叉树-广度优先遍历.py
@@ -0,0 +1,29 @@
+# 利用长度法
+# Definition for a binary tree node.
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+        queue = collections.deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result.append(level)
+        return result
